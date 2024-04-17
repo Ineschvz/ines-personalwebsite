ae69995f058ad851a263fa9473a3dfdfd61508fb
Ines Chavez <ineschvz21@gmail.com>
2024-04-16 22:11:46 -0400

    server is up and running on localhost:8080

---
+++ client/index.js
@@ -0,0 +1,14 @@
+//here we will mount and render the main react components on to the "root" element 
+import React from 'react';
+import {render} from 'react-dom';
+// import App from './App.js'
+import {createRoot} from 'react-dom/client'
+// import styles from './styles/style.css'
+
+const domNode = document.getElementById('root');
+const root = createRoot (domNode);
+
+root.render(
+    <div> hi there </div>
+  );
+  
